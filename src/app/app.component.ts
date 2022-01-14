68359bd643a7afde92ac6abf17f123925bd7029c
maurilioSousa-Desing <fmaurilio6@gmail.com>
2022-01-14 19:42:17 -0300

    Create app.component.ts

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Sistema de Gestão Para Igrejas';
+  description = 'Bem Vindo a Paz Church Jaibaras';
+}
